1d504eacb1e09e8dd2c62e16f88f36b52bdcc89d
Ulises Tadeo Perez Garcia <ulisessykes8@gmail.com>
2021-12-03 21:02:46 -0600

    Creamos las excepciónes en editar.

--- U3RazasPerros/Areas/Admin/Controllers/RazasController.cs
+++ U3RazasPerros/Areas/Admin/Controllers/RazasController.cs
@@ -24,7 +24,7 @@ namespace U3RazasPerros.Areas.Admin.Controllers
             Host = host;
         }
 
-        
+
 
         [Route("admin/Razas")]
         [Route("admin/Razas/Index")]
@@ -32,7 +32,7 @@ namespace U3RazasPerros.Areas.Admin.Controllers
         public IActionResult Index()
         {
             var razas = Context.Razas.OrderBy(x => x.Nombre);
-        
+
             return View(razas);
         }
         [HttpGet]
@@ -64,7 +64,8 @@ namespace U3RazasPerros.Areas.Admin.Controllers
             {
                 ModelState.AddModelError("", "El peso mínimo del perro no puede ser mayor al peso máximo");
             }
-            else if (vm.Razas.PesoMax < vm.Razas.PesoMin) {
+            else if (vm.Razas.PesoMax < vm.Razas.PesoMin)
+            {
                 ModelState.AddModelError("", "El peso máximo del perro no puede ser menor al peso mínimo");
             }
             else if (vm.Razas.PesoMin == vm.Razas.PesoMax || vm.Razas.PesoMax == vm.Razas.PesoMin)
@@ -79,11 +80,11 @@ namespace U3RazasPerros.Areas.Admin.Controllers
             {
                 ModelState.AddModelError("", "La altura máxima no puede ser menor ser menor a la altura mínima del perro");
             }
-            else if (vm.Razas.EsperanzaVida == 0) 
+            else if (vm.Razas.EsperanzaVida == 0)
             {
                 ModelState.AddModelError("", "La esperanza de vida no puede ser 0 años. ");
             }
-            else if (vm.Razas.EsperanzaVida >=100)
+            else if (vm.Razas.EsperanzaVida >= 100)
             {
                 ModelState.AddModelError("", "La esperanza de vida no puede ser mayor o igual a 100 años");
             }
@@ -135,7 +136,7 @@ namespace U3RazasPerros.Areas.Admin.Controllers
             }
             vm.Paises = Context.Paises.OrderBy(x => x.Nombre);
             return View(vm);
-            
+
         }
 
         #region Editar
@@ -159,57 +160,121 @@ namespace U3RazasPerros.Areas.Admin.Controllers
         {
             if (string.IsNullOrWhiteSpace(vm.Razas.Nombre))
             {
-                ModelState.AddModelError("", "El Nonbre puede estar en blanco");
-                return View(vm);
+                ModelState.AddModelError("", "Todas las razas de perros debe de tener un nombre");
             }
-
-            if (archivo1 != null)
+            else if (string.IsNullOrWhiteSpace(vm.Razas.Descripcion))
             {
-                if (archivo1.ContentType != "image/jpeg") //tipo mime
+                ModelState.AddModelError("", "La descripción del dogo no puede estar en blanco, por favor describe a tu dogo uwu");
+            }
+            else if (string.IsNullOrWhiteSpace(vm.Razas.OtrosNombres))
+            {
+                ModelState.AddModelError("", "No se puede dejar en blanco, los dogos deben de tener otro nombre.");
+            }
+            else if (vm.Razas.PesoMin > vm.Razas.PesoMax)
+            {
+                ModelState.AddModelError("", "El peso mínimo del perro no puede ser mayor al peso máximo");
+            }
+            else if (vm.Razas.PesoMax < vm.Razas.PesoMin)
+            {
+                ModelState.AddModelError("", "El peso máximo del perro no puede ser menor al peso mínimo");
+            }
+            else if (vm.Razas.PesoMin == vm.Razas.PesoMax || vm.Razas.PesoMax == vm.Razas.PesoMin)
+            {
+                ModelState.AddModelError("", "El peso mínimo y el peso máximo no pueden ser iguales");
+            }
+            else if (vm.Razas.AlturaMin > vm.Razas.AlturaMax)
+            {
+                ModelState.AddModelError("", "La altura mínima del perro no puede ser mayor a la altura maxima");
+            }
+            else if (vm.Razas.AlturaMax < vm.Razas.AlturaMin)
+            {
+                ModelState.AddModelError("", "La altura máxima no puede ser menor ser menor a la altura mínima del perro");
+            }
+            else if (vm.Razas.EsperanzaVida == 0)
+            {
+                ModelState.AddModelError("", "La esperanza de vida no puede ser 0 años. ");
+            }
+            else if (vm.Razas.EsperanzaVida >= 100)
+            {
+                ModelState.AddModelError("", "La esperanza de vida no puede ser mayor o igual a 100 años");
+            }
+            else if (vm.Razas.AlturaMin == vm.Razas.AlturaMax && vm.Razas.AlturaMax == vm.Razas.AlturaMin)
+            {
+                ModelState.AddModelError("", "La altura mínima y la altura máxima no pueden ser iguales.");
+            }
+            else if (string.IsNullOrWhiteSpace(vm.Razas.Caracteristicasfisicas.Patas))
+            {
+                ModelState.AddModelError("", "El campo patas no puede estar en blanco.");
+            }
+            else if (string.IsNullOrWhiteSpace(vm.Razas.Caracteristicasfisicas.Cola))
+            {
+                ModelState.AddModelError("", "El campo cola no puede estar en blanco.");
+            }
+            else if (string.IsNullOrWhiteSpace(vm.Razas.Caracteristicasfisicas.Hocico))
+            {
+                ModelState.AddModelError("", "El campo Hocico no puede estar en blanco.");
+            }
+            else if (string.IsNullOrWhiteSpace(vm.Razas.Caracteristicasfisicas.Pelo))
+            {
+                ModelState.AddModelError("", "El campo pelo no puede estar en blanco.");
+            }
+            else if (string.IsNullOrWhiteSpace(vm.Razas.Caracteristicasfisicas.Color))
+            {
+                ModelState.AddModelError("", "El campo color no puede estar en blanco.");
+            }
+            else
+            {
+                if (archivo1 != null)
                 {
-                    ModelState.AddModelError("", "Solo se permite la carga de imagen en formato jpg");
-                    return View(vm);
+                    if (archivo1.ContentType != "image/jpeg") //tipo mime
+                    {
+                        ModelState.AddModelError("", "Solo se permite la carga de imagen en formato jpg");
+                        return View(vm);
+                    }
                 }
-            }
 
-            var Editarcat = Context.Caracteristicasfisicas.FirstOrDefault(x => x.Id == vm.Razas.Id);
-            var EditarRas = Context.Razas.FirstOrDefault(x => x.Id == vm.Razas.Id);
-            if (EditarRas == null || Editarcat == null)
-            {
-                RedirectToAction("Index");
-            }
+                var Editarcat = Context.Caracteristicasfisicas.FirstOrDefault(x => x.Id == vm.Razas.Id);
+                var EditarRas = Context.Razas.FirstOrDefault(x => x.Id == vm.Razas.Id);
+                if (EditarRas == null || Editarcat == null)
+                {
+                    RedirectToAction("Index");
+                }
 
-            EditarRas.IdPais = vm.Razas.IdPais;
-            EditarRas.Nombre = vm.Razas.Nombre;
-            EditarRas.Descripcion = vm.Razas.Descripcion;
-            EditarRas.OtrosNombres = vm.Razas.OtrosNombres;
-            EditarRas.PesoMin = vm.Razas.PesoMin;
-            EditarRas.PesoMax = vm.Razas.PesoMax;
-            EditarRas.AlturaMax = vm.Razas.AlturaMax;
-            EditarRas.AlturaMin = vm.Razas.AlturaMin;
-            EditarRas.EsperanzaVida = vm.Razas.EsperanzaVida;
-            Editarcat.Patas = vm.Razas.Caracteristicasfisicas.Patas;
-            Editarcat.Cola = vm.Razas.Caracteristicasfisicas.Cola;
-            Editarcat.Hocico = vm.Razas.Caracteristicasfisicas.Hocico;
-            Editarcat.Pelo = vm.Razas.Caracteristicasfisicas.Pelo;
-            Editarcat.Color = vm.Razas.Caracteristicasfisicas.Color;
-            Context.Update(EditarRas);
-            Context.Update(Editarcat);
-            Context.SaveChanges();
+                EditarRas.IdPais = vm.Razas.IdPais;
+                EditarRas.Nombre = vm.Razas.Nombre;
+                EditarRas.Descripcion = vm.Razas.Descripcion;
+                EditarRas.OtrosNombres = vm.Razas.OtrosNombres;
+                EditarRas.PesoMin = vm.Razas.PesoMin;
+                EditarRas.PesoMax = vm.Razas.PesoMax;
+                EditarRas.AlturaMax = vm.Razas.AlturaMax;
+                EditarRas.AlturaMin = vm.Razas.AlturaMin;
+                EditarRas.EsperanzaVida = vm.Razas.EsperanzaVida;
+                Editarcat.Patas = vm.Razas.Caracteristicasfisicas.Patas;
+                Editarcat.Cola = vm.Razas.Caracteristicasfisicas.Cola;
+                Editarcat.Hocico = vm.Razas.Caracteristicasfisicas.Hocico;
+                Editarcat.Pelo = vm.Razas.Caracteristicasfisicas.Pelo;
+                Editarcat.Color = vm.Razas.Caracteristicasfisicas.Color;
+                Context.Update(EditarRas);
+                Context.Update(Editarcat);
+                Context.SaveChanges();
 
-            if (archivo1 != null)
-            {
-                var path = Host.WebRootPath + "/imgs_perros/" + vm.Razas.Id + "_0.jpg";
-                FileStream fs = new FileStream(path, FileMode.Create);
-                archivo1.CopyTo(fs);
-                fs.Close();
+                if (archivo1 != null)
+                {
+                    var path = Host.WebRootPath + "/imgs_perros/" + vm.Razas.Id + "_0.jpg";
+                    FileStream fs = new FileStream(path, FileMode.Create);
+                    archivo1.CopyTo(fs);
+                    fs.Close();
+                }
+                return RedirectToAction("Index");
             }
-            return RedirectToAction("Index");
+            vm.Paises = Context.Paises.OrderBy(x => x.Nombre);
+            return View(vm);
+
         }
         #endregion
 
         [HttpGet]
-        public IActionResult Eliminar (int id)
+        public IActionResult Eliminar(int id)
         {
             var raza = Context.Razas.FirstOrDefault(x => x.Id == id);
             var caract = Context.Caracteristicasfisicas.FirstOrDefault(x => x.Id == id);
@@ -219,11 +284,11 @@ namespace U3RazasPerros.Areas.Admin.Controllers
                 return RedirectToAction("Index");
             }
             return View(raza);
-            
+
         }
 
         [HttpPost]
-        public IActionResult Eliminar(Razas r, Caracteristicasfisicas c )
+        public IActionResult Eliminar(Razas r, Caracteristicasfisicas c)
         {
             var raza = Context.Razas.FirstOrDefault(x => x.Id == r.Id);
             var carac = Context.Caracteristicasfisicas.FirstOrDefault(x => x.Id == c.Id);
